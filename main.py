83f677d8622562945bb61c5c2e90533e4993327e
JuverdeanuDianaMaria <diana.juverdeanu@stud.electro.upb.ro>
2026-06-26 16:17:35 +0300

    Updated products on main branch

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from product import Product
 from product_manager import ProductManager
 
+
 manager = ProductManager()
 
-p1 = Product("Laptop", 3000, 2)
-p2 = Product("Mouse", 100, 5)
-p3 = Product("Keyboard", 200, 3)
+p1 = Product("Gaming Laptop", 3000, 1)
+p2 = Product("Wireless Mouse", 100, 2)
+p3 = Product("Mechanical Keyboard", 200, 1)
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
-
-manager.display_products()
-
-print("Valoarea totala:", manager.total_value(), "lei")
